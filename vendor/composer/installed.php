79dda03d73d328e71c3541ab59f75c283e8227a8
Kyllian-MATHIEU <113108888+Kyllian-MATHIEU@users.noreply.github.com>
2023-01-13 11:28:16 +0100

    v1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/con_foot',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ac606b54e48dba9731ac5a9b64df91d9c3bc76dd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/con_foot' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ac606b54e48dba9731ac5a9b64df91d9c3bc76dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
