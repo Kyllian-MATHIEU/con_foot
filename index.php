fc46624000c7bcdf40381e2b94a5766cdde2a1f7
Kyllian-MATHIEU <113108888+Kyllian-MATHIEU@users.noreply.github.com>
2023-01-26 13:26:21 +0100

    semaine 26/01/2023

--- index.php
+++ index.php
@@ -8,8 +8,22 @@ echo $giroud->getPrenom()."\n";
 echo $giroud->getDateNaissance()->format("d/m/Y")."\n";
 echo $giroud->donneTexte()."\n";
 
+use App\class\Selectionneur;
+$Deschamps = new Selectionneur("Deschamps", "Didier", new DateTime("1968-10-15"));
+echo $Deschamps->getNom()."\n";
+echo $Deschamps->getPrenom()."\n";
+echo $Deschamps->getDateNaissance()->format("d/m/Y")."\n";
+echo $Deschamps->donneTexte()."\n";
+
+use App\class\Joueur;
+$mbappe = new Joueur("Mbappe", "Kylian", new DateTime("1998-12-20"), "10");
+echo $mbappe->getNom()."\n";
+echo $mbappe->getPrenom()."\n";
+echo $mbappe->getDateNaissance()->format("d/m/Y")."\n";
+echo $mbappe->getNumMaillot()."\n";
+echo $mbappe->donneTexte()."\n";
 
 
-
-
-
+use App\class\Stade;
+$stade = new Stade( "PalaisDesSport");
+echo $stade->getNom()."\n";
